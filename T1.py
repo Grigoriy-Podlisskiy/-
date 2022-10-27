4255ffe793441eb76002052bc0c98e64c2a5e8bf
dell <grina_24@mail.ru>
2022-10-27 21:45:16 +0300

    homework

---
+++ T1.py
@@ -0,0 +1,20 @@
+money_capital = 10000  # финансовая подушка безопасности!!!
+salary = 5000          # ежемесячная зарплата!!! приток 
+spend = 6000          # расходы на проживание
+increase = 0.05      # ежемесячный рост цен
+
+month = 0  # количество месяцев, которое можно прожить
+
+# TODO Оформить решение
+while money_capital + salary > spend:
+    money_capital -= spend
+    month += 1
+    spend = spend + spend * increase
+    money_capital = money_capital + salary - spend
+    if money_capital > 0:
+        month += 1
+        spend = spend + spend * increase
+        money_capital += salary
+
+
+print(month)
